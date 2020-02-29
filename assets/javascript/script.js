8c47602d7d194bfe45982f4f89d28d4054904312
kitt mattison <kittmatt1@gmail.com>
2020-02-28 23:49:31 -0800

    Js linked to html framework

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@
         
  var wins = 0; 
  var losses = 0;
- var remainingGuesses = 0;
+ var remainingGuesses =12;
  var mistakesMade=0;
  var lettersGuessed = [];
  var answerArray = [];
@@ -21,60 +21,57 @@
          "sequoia",
  ];
   
- function initializeGame() {
-         lettersGuessed = [] {
-                 lettersGuessed = [];
-                 answerArray = [];
-                 remainingLetters = 0;
-                 currentWord = wordBank[Math.floor(Math.random() * (wordBank.length))];
- 
-                 for (var i = 0; i < currentWord.length; i++) {
+        function initializeGame( ) {
+                lettersGuessed = [ ];
+                answerArray = [ ];
+                remainingLetters = 0;
+                currentWord = wordBank[Math.floor(Math.random() * (wordBank.length))];
+                                
+                        
+                for (var i = 0; i < currentWord.length; i++) {
                          answerArray.push("_");
                          console.log(currentWord);
- 
-                 }
-                 }
- }
- 
- $(doucment).ready();
-  $(document).on('keypress' , function(event) {
-                 var userGuess = event.key.tolowercase();
-  
- }
- ) 
- lettersGuessed = [];
- answerArrray = [];
- remainingLetters
- }
- 
-         for (var k = 0; k < lettersGuessed.length; k++) {
-                 if (lettersGuessed[k] === userGuess) {
-                 alert('You already picked that!')
-                 }
-         }
- 
-         lettersGuessed.push(userGuess);
-                 $("#lettersGuessed").text(lettersGuessed);
-         
-         
-         currentWord.indexOf(userGuess);
-         if (currentWord.indexOf(userGuess) !== -1){
- 
-         for (var j = 0; j < currentWord.length; j++) {
-                 if (currentWord[j] === userGuess) {
-                         answerArray[j] = userGuess;
-                         if (remainingLetters === 0 ) { 
-                 wins++
-                 initialiazeGame();
-         }
- }
- }
- } else {mistakesMade++;
-         reaminingGuesses--;                
-         }
-         }
-         }
-         }
- {}
+                }
+                        remainingLetters = currentWord.length;
+                        $("#answer").text(answerArray);
+                };
+                
+                initializeGame(); 
+
+                $(document).ready();
+                        $(document).on('keypress' , function(event) {
+                                var userGuess = event.key.toLowercase();
+                                console.log('user guess', userGuess)
+                                if (remainingGuesses > 0) {
+
+                                lettersGuessed.push(userGuess);
+                                        $("#lettersGuessed").text(lettersGuessed);
+                                        
+                                        
+                                currentWord.indexOf(userGuess);
+                                        if (currentWord.indexOf(userGuess) !== -1) {
+                                        
+                                ['yosemite']
+                                                'y' - 0, 'o' - 0, 's' - 0, 'e' - 0, 'm' - 0, 'i' - 0, 't' - 0, 'e' - 0,
+                                                
+                                        for  (var j = 0; j < currentWord.length; j++) {
+                                                if (currentWord[j] === userGuess) {
+                                                        answerArray[j] = userGuess;
+                                                        remainingLetters--
+                                                        if (remainingLetters === 0 ) { 
+                                                wins++
+                                                initialiazeGame();
+                                        }
+                                }
+                        }
+                                        } else {
+                                                mistakesMade++
+                                                reaminingGuesses--;        
+                                        }
+                                }
+                        }
+                
+               
+        }];
          
  
